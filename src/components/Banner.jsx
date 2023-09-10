775cccbea99c08c0bb23cae004dbb13cbdd1f763
Ansh Varma <96176123+anshvarma07@users.noreply.github.com>
2023-09-10 15:40:26 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
 
 export default function Banner() {
   return (
@@ -8,7 +8,8 @@ export default function Banner() {
             <div className='text-center md:text-4xl  text-2xl'>Learn with us.</div>
             <div className='text-center text-white md:text-7xl text-3xl'>Grow with us.</div>
             <div className='text-center md:text-3xl'>
-            <Typed strings={[' Hello Nigga!',' Hello Paaji!']} typeSpeed={60} backSpeed={90} loop/>
+            {/* <Typed strings={[' Hello Nigga!',' Hello Paaji!']} typeSpeed={60} backSpeed={90} loop/> */}
+            Hello Paaji!
             </div>
         </div>
       
