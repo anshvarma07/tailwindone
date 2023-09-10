be3c0386cd5e1cd53c75ebe77b831b4c60a9afe7
Ansh Varma <96176123+anshvarma07@users.noreply.github.com>
2023-09-10 19:53:27 +0530

    Started Newsletter Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
 import Experts from "./components/Experts";
+import Newsletter from './components/Newsletter'
 
 function App() {
   return <div className="App">
     <Header />
     <Banner />
     <Experts />
+    <Newsletter />
   </div>;
 }
 
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Newsletter() {
+  return (
+    <div className='h-[200px] bg-[#2699fb]'>
+        <div className='max-w-[1240px] grid grid-cols-3 mx-auto border border-black'>
+            <div className="text-lg col-span-2 border border-blue-100 text-white p-10">
+                <h2>Want to learn latest IT skills?</h2>
+                <p>Sign Up to our Newsletter to stay updated.</p>
+                
+            </div>
+            <div className="text-lg col-span-1 border border-blue-200">Hello</div>
+        </div>
+      
+    </div>
+  )
+}
