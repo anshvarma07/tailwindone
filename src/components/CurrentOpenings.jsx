98ad7d7d03588fefbc4cf80bd8ca3c80441bca74
Ansh Varma <ansh.varma7@gmail.com>
2024-11-17 15:16:48 +0530

    Close posn

--- src/components/CurrentOpenings.jsx
+++ src/components/CurrentOpenings.jsx
@@ -72,12 +72,12 @@ const CurrentOpenings = () => {
                   <span>+91 9266532527</span>
                 </div>
                 <a 
-                  href="https://forms.gle/dCc4ZkizHfEAvmK49" 
-                  target="_blank" 
+                  href="" // href="https://forms.gle/dCc4ZkizHfEAvmK49" 
+                  // target="_blank" 
                   rel="noopener noreferrer" 
                   className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-300 w-full sm:w-auto"
                 >
-                  Apply Now!
+                  {/* Apply Now! */}Fullfilled
                 </a>
               </div>
             </div>
